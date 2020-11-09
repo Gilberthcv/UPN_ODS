bef1af9ce249a61b69141b7ce353843e2df6a1fc
Gilberth Contreras <gilberth.contreras@upn.edu.pe>
2020-11-08 19:54:11 -0500

    Update AC045.sql

--- Querys/AC045.sql
+++ Querys/AC045.sql
@@ -1,9 +1,10 @@
---AC045
+--AC045_
 SELECT --A.*, C.REGISTRATION_STATUS 
 	D.CAMPUS, B.SPRIDEN_ID, B.SPRIDEN_LAST_NAME ||' '|| B.SPRIDEN_FIRST_NAME AS APELLIDOS_NOMBRES, A.SFRREGP_TERM_CODE AS PERIODO_PROYECCION
 	, A.SFRREGP_SUBJ_CODE||A.SFRREGP_CRSE_NUMB_LOW AS CURSO_PROYECCION, CASE WHEN C.PERSON_UID IS NOT NULL THEN 'Y' ELSE 'N' END AS MATRICULADO
 	, A.SHRTCKN_SUBJ_CODE||A.SHRTCKN_CRSE_NUMB AS CURSO_APROBADO, A.SHRTCKG_GRDE_CODE_FINAL AS NOTA, A.SHRTCKN_TERM_CODE AS PERIODO_APROBO
 	, A.SFRREGP_USER_ID AS USUARIO_AUTORIZO, A.SPRIDEN_LAST_NAME ||' '|| A.SPRIDEN_FIRST_NAME AS APELLIDOS_NOMBRES_USUARIO
+	, CASE WHEN A.SFRREGP_MAINT_IND = 'U' THEN 'MANUAL' ELSE 'SISTEMA' END AS SFRREGP_MAINT_IND
 FROM (
 	SELECT SFRREGP_PIDM,
 		SFRREGP_SUBJ_CODE,
@@ -15,7 +16,8 @@ FROM (
 		shrtckn_term_code,
 		sfrregp_user_id,
 		spriden_last_name,
-		spriden_first_name
+		spriden_first_name,
+		sfrregp_maint_ind
 		--COUNT(distinct SHRTCKN_TERM_CODE)
 	FROM ODSMGR.SFRREGP, ODSMGR.SHRTCKN, ODSMGR.SCREQIV, ODSMGR.SHRTCKG, ODSMGR.LOE_SPRIDEN, ODSMGR.SFRSTCR, ODSMGR.LOE_SSBSECT
 	WHERE  shrtckn_pidm = sfrregp_pidm
@@ -27,7 +29,7 @@ FROM (
 		and sfrregp_crse_numb_low = ssbsect_crse_numb
 		AND substr(sfrregp_user_id,2,9) = spriden_id
 		--and sfrregp_pidm = 35137
-		AND SFRREGP_TERM_CODE IN ('220435','220534')
+		AND SFRREGP_TERM_CODE IN (/*'220435',*/'220534')
 		And shrtckn_pidm = shrtckg_pidm 
 		and shrtckn_term_code = shrtckg_term_code
 		and shrtckn_seq_no = shrtckg_tckn_seq_no
@@ -39,7 +41,7 @@ FROM (
 		and 
 		--shrtckn_subj_code||shrtckn_crse_numb||shrtckn_term_code between screqiv_subj_code_eqiv(+)||screqiv_crse_numb_eqiv (+)||screqiv_start_term (+) and screqiv_subj_code_eqiv(+)||screqiv_crse_numb_eqiv (+)||screqiv_end_term (+)
 		(shrtckn_subj_code = screqiv_subj_code_eqiv(+) AND shrtckn_crse_numb = screqiv_crse_numb_eqiv(+) AND shrtckn_term_code BETWEEN screqiv_start_term (+) and screqiv_end_term(+))
-		and sfrregp_maint_ind = 'U'
+		--and sfrregp_maint_ind = 'U'
 		and (shrtckg_grde_code_final in ('12','13','14','15','16','17','18','19','20', 'C') OR shrtckg_grde_code_final like 'C%' )
 		--and sfrregp_most_prob is not null
 	GROUP BY SFRREGP_PIDM,
@@ -52,7 +54,8 @@ FROM (
 	    shrtckn_term_code,
 	    sfrregp_user_id,
 	    spriden_last_name,
-	    spriden_first_name
+	    spriden_first_name,
+		sfrregp_maint_ind
 	) A
 		INNER JOIN ODSMGR.LOE_SPRIDEN B ON A.SFRREGP_PIDM = B.SPRIDEN_PIDM AND B.SPRIDEN_CHANGE_IND IS NULL
 		INNER JOIN ODSMGR.STUDENT_COURSE C ON A.SFRREGP_PIDM = C.PERSON_UID AND A.SFRREGP_TERM_CODE = C.ACADEMIC_PERIOD
